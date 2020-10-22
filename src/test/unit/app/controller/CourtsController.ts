e0f118af60a28b6b0a382adc8fe1abb4a21be8a3
velimd <duzgunce.vd@gmail.com>
2020-10-22 10:25:56 +0100

    test name clean up

--- src/test/unit/app/controller/CourtsController.ts
+++ src/test/unit/app/controller/CourtsController.ts
@@ -9,14 +9,14 @@ const i18n = {
 describe('Home Controller', () => {
   const controller = new CourtsController();
 
-  test('Should render the login page', async () => {
+  test('Should redirect to the login page', async () => {
     const req = mockRequest(i18n);
     const res = mockResponse();
     await controller.get(req, res);
     expect(res.redirect).toBeCalledWith('/login');
   });
 
-  test('Should render the login page', async () => {
+  test('Should render the courts page', async () => {
     const req = mockRequest(i18n);
     req.user = true;
     const res = mockResponse();
