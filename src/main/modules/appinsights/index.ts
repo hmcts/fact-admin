3cfb4ddc4d1342541b5ad8ca87c00837547d6e67
Linus Norton <linusnorton@gmail.com>
2020-10-27 07:01:52 +0000

    update appinsights config

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -7,13 +7,10 @@ export class AppInsights {
   enableFor(server: Application): void {
     if (config.get('appInsights.instrumentationKey')) {
       appInsights.setup(config.get('appInsights.instrumentationKey'))
-        .setAutoDependencyCorrelation(true)
-        .setAutoCollectRequests(true)
-        .setAutoCollectPerformance(true)
-        .setAutoCollectDependencies(true)
-        .setAutoCollectConsole(true, true)
+        .setSendLiveMetrics(true)
         .start();
 
+      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'Admin';
       appInsights.defaultClient.trackTrace({message: 'App insights activated'});
     }
   }
