d6394ee646ebcd127f8bf4dc6f065cc57e2c8e9d
Linus Norton <linusnorton@gmail.com>
2020-10-29 20:57:02 +0000

    remove debug, correct appinsights key path

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -12,7 +12,7 @@ export class PropertiesVolume {
       set(config, 'services.idam.clientSecret', get(config, 'secrets.fact.oauth-client-secret'));
       set(config, 'session.redis.key', get(config, 'secrets.fact.redis-access-key'));
       set(config, 'session.secret', get(config, 'secrets.fact.redis-access-key'));
-      set(config, 'applicationInsights.instrumentationKey', get(config, 'secrets.fact.AppInsightsInstrumentationKey'));
+      set(config, 'appInsights.instrumentationKey', get(config, 'secrets.fact.AppInsightsInstrumentationKey'));
     }
   }
 }
