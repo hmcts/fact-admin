5fad06cf600bda4eb6a6eee89f5a13e79f99c81d
Linus Norton <linusnorton@gmail.com>
2020-10-29 21:54:52 +0000

    Add file

---
+++ src/main/modules/passport/index.ts
@@ -0,0 +1,70 @@
+import {Application, NextFunction, Request, Response} from 'express';
+import {asClass, asValue} from 'awilix';
+import Axios from 'axios';
+import config from 'config';
+import {FactApi} from '../../app/fact/FactApi';
+import {AuthedRequest} from '../../types/AuthedRequest';
+
+/**
+ * Adds the passport middleware to add oauth authentication
+ */
+export class OidcMiddleware {
+
+  public enableFor(server: Application): void {
+    const loginUrl: string = config.get('services.idam.authorizationURL');
+    const tokenUrl: string = config.get('services.idam.tokenURL');
+    const clientId: string = config.get('services.idam.clientID');
+    const clientSecret: string = config.get('services.idam.clientSecret');
+    const redirectUri: string = config.get('services.idam.callbackURL');
+
+    server.get('/login', (req, res) => {
+      res.redirect(loginUrl + '?client_id=' + clientId + '&response_type=code&redirect_uri=' + encodeURI(redirectUri));
+    });
+
+    server.get('/oauth2/callback', async (req: Request, res: Response) => {
+      const response = await Axios.post(
+        tokenUrl,
+        `client_id=${clientId}&client_secret=${clientSecret}&grant_type=authorization_code&redirect_uri=${encodeURIComponent(redirectUri)}&code=${encodeURIComponent(req.query.code as string)}`,
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+      );
+
+      req.session.user = response.data;
+      res.render('redirect');
+    });
+
+    server.get('/logout', function(req, res){
+      req.session.user = undefined;
+      res.render('logout');
+    });
+
+    server.use((req: AuthedRequest, res: Response, next: NextFunction) => {
+      console.log(req.session);
+      if (req.session.user) {
+        req.scope = req.app.locals.container.createScope();
+        req.scope.register({
+          axios: asValue(Axios.create({
+            baseURL: config.get('services.api.url'),
+            headers: {
+              Authorization: 'Bearer ' + req.session.user.id_token
+            }
+          })),
+          api: asClass(FactApi)
+        });
+
+        return next();
+      }
+      res.redirect('/login');
+    });
+
+  }
+
+}
+
+export type AuthedUser = {
+  id_token: string
+}
