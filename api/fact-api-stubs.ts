6b58f5828d1aa02cb3f1b0e1aefdcaee8069e0d2
velimd <duzgunce.vd@gmail.com>
2020-11-01 17:56:01 +0000

    update stub endpoints

--- api/fact-api-stubs.ts
+++ api/fact-api-stubs.ts
@@ -5,21 +5,22 @@ const app = express();
 const data = require('./courts.json');
 const courtDetails = require('./court-details.json');
 const port = 8080;
+const baseURL = '/admin/courts';
 
 app.use(bodyParser.json());
 
-app.get('/courts/all', (req: Request, res: Response) => {
+app.get(`${baseURL}/all`, (req: Request, res: Response) => {
   res.json(data);
 });
 
-app.get('/courts/:slug', (req: Request, res: Response) => {
+app.get(`${baseURL}/:slug`, (req: Request, res: Response) => {
   const slug: string = (req.params.slug as string).toLowerCase();
   const courts = [...courtDetails];
   const court = courts.find(court => court.slug.toLowerCase() === slug);
   res.json(court);
 });
 
-app.put('/courts/:slug', (req: Request, res: Response) => {
+app.put(`${baseURL}/:slug`, (req: Request, res: Response) => {
   const slug: string = (req.params.slug as string).toLowerCase();
   const updatedCourt = req.body;
   const courtIndex = courtDetails.findIndex((court: any)=> court.slug.toLowerCase() === slug);
