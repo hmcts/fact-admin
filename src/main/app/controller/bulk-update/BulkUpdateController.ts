74efd3eced1891c45734cb5f4cc195952e823745
Linus Norton <linusnorton@gmail.com>
2020-11-25 21:02:59 +0000

    make form work

--- src/main/app/controller/bulk-update/BulkUpdateController.ts
+++ src/main/app/controller/bulk-update/BulkUpdateController.ts
@@ -13,9 +13,21 @@ export class BulkUpdateController {
   }
 
   public async post(req: AuthedRequest, res: Response): Promise<void> {
-    console.log(req.body);
+    let error = false;
+
+    if (req.body.courts.length > 0) {
+      try {
+        await req.scope.cradle.api.updateCourtsInfo({
+          'info': req.body.info_message,
+          'info_cy': req.body.info_message_cy,
+          'courts': typeof req.body.courts === 'string' ? [req.body.courts] : req.body.courts
+        });
+      } catch (err) {
+        error = true;
+      }
+    }
     const courts = await req.scope.cradle.api.getCourts();
 
-    res.render('bulk-update/index', { courts });
+    res.render('bulk-update/index', { courts, error });
   }
 }
