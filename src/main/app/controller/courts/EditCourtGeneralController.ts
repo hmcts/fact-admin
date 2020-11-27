d2fd5de06e699a9c0b431f7cd5ee2be982407cac
velimd <duzgunce.vd@gmail.com>
2020-11-27 17:09:40 +0000

    converting string boolean to actual boolean

--- src/main/app/controller/courts/EditCourtGeneralController.ts
+++ src/main/app/controller/courts/EditCourtGeneralController.ts
@@ -21,11 +21,21 @@ export class EditCourtGeneralController {
 
   public async post(req: AuthedRequest, res: Response): Promise<void> {
     const court = req.body;
+    this.convertOpenAndAccessSchemeToBoolean(court);
     const slug: string = req.params.slug as string;
     const updatedCourts = await req.scope.cradle.api.updateCourtGeneral(slug, court);
     if (isObjectEmpty(updatedCourts)) {
       return res.redirect(`/courts/${slug}/edit/general?updated=false`);
     }
     return res.redirect(`/courts/${slug}/edit/general?updated=true`);
+  }
+
+  private convertOpenAndAccessSchemeToBoolean(court: any): void {
+    if (court.open) {
+      court.open = court.open === 'true';
+    }
+    if (court['access_scheme']) {
+      court['access_scheme'] = court['access_scheme'] === 'true';
+    }
   }
 }
