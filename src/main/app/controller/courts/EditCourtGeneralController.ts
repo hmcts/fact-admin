4349d44e5d5be261f1d32ceb97326c72a81c6ba1
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-30 13:05:44 +0000

    disable access scheme if not inperson court

--- src/main/app/controller/courts/EditCourtGeneralController.ts
+++ src/main/app/controller/courts/EditCourtGeneralController.ts
@@ -33,9 +33,13 @@ export class EditCourtGeneralController {
   private convertOpenAndAccessSchemeToBoolean(court: any): void {
     if (court.open) {
       court.open = court.open === 'true';
+    } else {
+      court.open = false;
     }
     if (court['access_scheme']) {
       court['access_scheme'] = court['access_scheme'] === 'true';
+    } else {
+      court['access_scheme'] = false;
     }
   }
 }
