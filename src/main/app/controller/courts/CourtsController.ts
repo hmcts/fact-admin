d9ed6cce2cfb7115609458600c567ecf9f1dacfd
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-01-20 17:35:38 +0000

    FACT-166: fix url for aat and preview. Probably.

--- src/main/app/controller/courts/CourtsController.ts
+++ src/main/app/controller/courts/CourtsController.ts
@@ -1,6 +1,7 @@
 import autobind from 'autobind-decorator';
 import { Response } from 'express';
 import { AuthedRequest } from '../../../types/AuthedRequest';
+import config from 'config';
 
 @autobind
 export class CourtsController {
@@ -10,6 +11,7 @@ export class CourtsController {
    */
   public async get(req: AuthedRequest, res: Response): Promise<void> {
     const courts = await req.scope.cradle.api.getCourts();
+    res.locals.frontEndUrl = config.get('services.frontend.url');
 
     res.render('courts/courts', { courts });
   }
