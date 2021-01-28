cdc6ef0341c910e4f2279eab59ef55acb70a3d0a
Linus Norton <linusnorton@gmail.com>
2021-01-28 19:33:37 +0000

    change the way vault secrets are loaded

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -2,6 +2,7 @@ import config from 'config';
 import * as propertiesVolume from '@hmcts/properties-volume';
 import { Application } from 'express';
 import { get, set } from 'lodash';
+import { execSync } from 'child_process';
 
 export class PropertiesVolume {
 
@@ -13,6 +14,18 @@ export class PropertiesVolume {
       set(config, 'session.redis.key', get(config, 'secrets.fact.redis-access-key'));
       set(config, 'session.secret', get(config, 'secrets.fact.redis-access-key'));
       set(config, 'appInsights.instrumentationKey', get(config, 'secrets.fact.AppInsightsInstrumentationKey'));
+    } else {
+      this.setLocalSecret('oauth-client-secret', 'services.idam.clientSecret');
+
     }
+  }
+
+  /**
+   * Load a secret from the AAT vault using azure cli
+   */
+  private setLocalSecret(secret: string, toPath: string): void {
+    const result = execSync('az keyvault secret show --vault-name fact-aat -o tsv --query value --name ' + secret);
+
+    set(config, toPath, result.toString().replace('\n', ''));
   }
 }
