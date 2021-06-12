d34765445afd161efc237982a3c69693f40e2922
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-06-12 22:51:13 +0100

    launchdarkly-feature - Added in terraform/azure secret acquisition

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -9,15 +9,18 @@ export class PropertiesVolume {
   enableFor(server: Application) {
     if (server.locals.ENV !== 'development') {
       propertiesVolume.addTo(config);
-
       set(config, 'services.idam.clientSecret', get(config, 'secrets.fact.oauth-client-secret'));
       set(config, 'session.redis.key', get(config, 'secrets.fact.redis-access-key'));
       set(config, 'session.secret', get(config, 'secrets.fact.redis-access-key'));
       set(config, 'appInsights.instrumentationKey', get(config, 'secrets.fact.AppInsightsInstrumentationKey'));
       set(config, 'csrf.tokenSecret', get(config, 'secrets.fact.csrf-token-secret'));
+      set(config, 'featureToggles.enabled', true);
+      set(config, 'launchDarkly.sdkKey', get(config, 'secrets.fact.launchdarkly-sdk-key'));
     } else {
+      set(config, 'featureToggles.enabled', true);
       this.setLocalSecret('oauth-client-secret', 'services.idam.clientSecret');
       this.setLocalSecret('csrf-token-secret', 'csrf.tokenSecret');
+      this.setLocalSecret('launchdarkly-sdk-key', 'launchDarkly.sdkKey');
     }
   }
 
