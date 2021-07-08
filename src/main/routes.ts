d0e057b2f6b308459cd8539f419e4188271be015
Thomas Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-07-08 15:52:20 +0100

    admin-cleanup - Removed unused controller/view

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -8,7 +8,6 @@ export default function(app: Application): void {
   app.post('/bulk-update', isSuperAdmin, app.locals.container.cradle.bulkUpdateController.post);
   app.get('/courts', app.locals.container.cradle.courtsController.get);
   app.get('/courts/download', app.locals.container.cradle.courtsDownloadController.get);
-  app.get('/courts/:slug', app.locals.container.cradle.courtDetailsController.get);
   app.get('/courts/:slug/edit/general', app.locals.container.cradle.editCourtController.get);
   app.get('/courts/:slug/general-info', app.locals.container.cradle.generalInfoController.get);
   app.put('/courts/:slug/general-info', app.locals.container.cradle.generalInfoController.put);
