684ac55fefbad493b45774138b27f9e1dabfaadd
amstevenson <addstevenson@hotmail.com>
2021-07-14 16:13:53 +0100

    search working for dynamic text and toggle

--- src/main/assets/js/courts.ts
+++ src/main/assets/js/courts.ts
@@ -4,6 +4,7 @@ export class CourtsController {
 
   private contentId = '#main-content';
   private toggleCourts = 'toggleClosedCourtsDisplay';
+  // private toggleCourtsId = '#toggleClosedCourtsDisplay';
   private searchCourtsFilter = 'searchCourts';
   private searchCourtsFilterId = '#searchCourts';
   private courtsHiddenId = 'courtsHidden';
@@ -12,13 +13,15 @@ export class CourtsController {
   private TABLE_HEADER = '    <table class="govuk-table" id="courtsResults">' +
     '      <thead class="govuk-table__head">' +
     '        <tr class="govuk-table__row">' +
-    '          <th scope="col" class="govuk-table__header">Name</th>' +
+    '          <th scope="col" class="govuk-table__header govuk-!-width-one-half">Name</th>' +
+    '          <th scope="col" class="govuk-table__header"></th>' +
     '          <th scope="col" class="govuk-table__header">Last Updated</th>' +
     '          <th scope="col" class="govuk-table__header"></th>' +
     '          <th scope="col" class="govuk-table__header"></th>' +
     '        </tr>' +
     '      </thead>';
   private TABLE_BODY_START = ' <tbody class="govuk-table__body">';
+  private TABLE_HEADER_BODY_END = '</tbody> </table>';
 
   constructor() {
     this.initialize();
@@ -26,7 +29,8 @@ export class CourtsController {
 
   private initialize(): void {
     $(() => {
-      this.setUpTableData((document.getElementById(this.courtsHiddenId) as HTMLInputElement).value);
+      this.setUpTableData((document.getElementById(this.courtsHiddenId) as HTMLInputElement).value,
+        '', false);
       this.setUpToggleClosedCourtsDisplay();
       this.setUpCourtsDynamicSearchFilter();
     });
@@ -35,84 +39,83 @@ export class CourtsController {
   private setUpToggleClosedCourtsDisplay(): void {
     $(this.contentId).on('change', `input[name=${this.toggleCourts}]`, e => {
       e.preventDefault();
-      console.log('test this hits');
+      this.setUpTableData((document.getElementById(this.courtsHiddenId) as HTMLInputElement).value,
+        $(this.searchCourtsFilterId).val() as string,
+        $('#main-content input[name="toggleClosedCourtsDisplay"]').prop('checked'));
     });
   }
 
   private setUpCourtsDynamicSearchFilter(): void {
     $(this.contentId).on('input', `input[name=${this.searchCourtsFilter}]`, e => {
       e.preventDefault();
-
-      const filterValue = $(this.searchCourtsFilterId).val();
-
-      const courts = (document.getElementById(this.courtsHiddenId) as HTMLInputElement).value;
-
-      const filteredCourtsList: (string | number | symbol)[] = [];
-      $.each(JSON.parse(courts),function(index,value){
-
-        if(value.name.toLowerCase().includes(filterValue.toString().toLowerCase())) {
-          console.log('match found!: ' + value);
-          filteredCourtsList.push(value);
-        }
-      });
-
+      this.setUpTableData((document.getElementById(this.courtsHiddenId) as HTMLInputElement).value,
+        $(this.searchCourtsFilterId).val() as string,
+        $('#main-content input[name="toggleClosedCourtsDisplay"]').prop('checked'));
     });
   }
 
-  private setUpTableData(courts: string) {
-
-    // const filterValue = $(this.searchCourtsFilterId).val();
-    //
+  private setUpTableData(courts: string, searchFilterValue: string, includeClosedCourts: boolean) {
 
     let tableBody = this.TABLE_HEADER + this.TABLE_BODY_START;
 
-    const string = this.parameterizedString('my name is %s1 and surname is %s2', ['John', 'Doe']);
-    console.log(string);
+    // const tableCourtData = this.getFilteredSearchResults(courts, searchFilterValue, includeClosedCourts) as unknown as string;
 
-    $.each(JSON.parse(courts).sort((a: { name: string }, b: { name: string }) =>
-      (a.name > b.name) ? 1 : -1),function(index,value) {
+    // sorting .sort((a: { name: string }, b: { name: string }) => (a.name > b.name) ? 1 : -1)
 
-      // if(filterValue && value.name.toLowerCase().includes(filterValue.toString().toLowerCase())) {
-      //   console.log('match found!: ' + value);
-      //   filteredCourtsList.push(value);
-      // }
-
-      const name = value.name;
-      const updatedAt = value.updated_at ?? '';
-      // const displayed = value.displayed;
-      const slug = value.slug;
-
-      tableBody += ' <tr class="govuk-table__row">' +
-        ' <td scope="row" class="govuk-table__cell">' + name + ' </td>' +
-        ' <td scope="row" class="govuk-table__cell">' + updatedAt + ' </td>' +
-        ' <td scope="row" class="govuk-table__cell">' +
-        '   <a id="view-"' + slug + ' class="govuk-link" href="/courts/"' + slug + '>view</a>' +
-        ' </td>' +
-        ' <td scope="row" class="govuk-table__cell">' +
-        '   <a id="edit-"' + slug + ' class="govuk-link" href="/courts/' + slug + '/edit/general?name=' + name + '">edit</a>' +
-        ' </td>' +
-        ' </tr>';
+    const filteredCourts = JSON.parse(courts).filter((a: { displayed: boolean }) => {
+      if (includeClosedCourts) {
+        return a.displayed == true || a.displayed == false;
+      }
+      else return a.displayed == true;
+    }).filter((a: { name: string }) => {
+      if (searchFilterValue.length == 0)
+        return a;
+      else if (a.name.toLowerCase().includes(searchFilterValue.toString().toLowerCase())) {
+        return a;
+      }
     });
 
-    tableBody += '</tbody> </table>';
+    tableBody += this.getCourtsTableBody(filteredCourts) + this.TABLE_HEADER_BODY_END;
 
     (document.getElementById(this.courtsResults) as HTMLElement).outerHTML = tableBody;
   }
 
-  /***
-   * @example parameterizedString("my name is %s1 and surname is %s2", "John", "Doe");
-   * @return "my name is John and surname is Doe"
-   *
-   * @firstArgument {String} like "my name is %s1 and surname is %s2"
-   * @otherArguments {String | Number}
-   * @returns {String}
-   */
-  private parameterizedString = (str: string, values: string[]) => {
-    if (!str) return '';
-    return str.replace(/%s[0-9]+/g, matchedStr => {
-      // const variableIndex = matchedStr.replace('%s', '') - 1;
-      const variableIndex = <any> matchedStr.replace('%s', '') -1;
-      return values[variableIndex];
+  // private getFilteredSearchResults(courts: string, filterValue: string,
+  //   includeClosedCourts: boolean): (string | number | symbol)[] {
+  //   const filteredCourtsList: (string | number | symbol)[] = [];
+  //
+  //   $.each(JSON.parse(courts),function(index, value){
+  //
+  //     // Conditions:
+  //     // 1. Check if the toggle box for 'include closed' is selected or not
+  //     // 2. Get the filter value, and search based on that if it is populated
+  //     // if(value.name.toLowerCase().includes(filterValue.toString().toLowerCase())) {
+  //     filteredCourtsList.push(value);
+  //     // }
+  //   });
+  //   return filteredCourtsList;
+  // }
+  // }
+
+  private getCourtsTableBody(filteredCourts: any) {
+    let tableData = '';
+    $.each(filteredCourts,function(index,value) {
+      function getDataStructure(name: string, updatedAt: string, displayed: string, slug: string): string {
+        return ' <tr class="govuk-table__row">' +
+          ' <td scope="row" class="govuk-table__cell">' + name + ' </td>' +
+          ' <td scope="row" class="govuk-table__cell">' + (displayed ? '' : 'closed') + ' </td>' +
+          ' <td scope="row" class="govuk-table__cell">' + updatedAt + ' </td>' +
+          ' <td scope="row" class="govuk-table__cell">' +
+          '   <a id="view-"' + slug + ' class="govuk-link" href="/courts/"' + slug + '>view</a>' +
+          ' </td>' +
+          ' <td scope="row" class="govuk-table__cell">' +
+          '   <a id="edit-"' + slug + ' class="govuk-link" href="/courts/' + slug + '/edit/general?name=' + name + '">edit</a>' +
+          ' </td>' +
+          ' </tr>';
+      }
+
+      tableData += getDataStructure(value.name, value.updated_at ?? '', value.displayed, value.slug);
     });
+    return tableData;
   }
 }
