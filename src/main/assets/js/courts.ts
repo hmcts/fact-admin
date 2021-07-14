cd1a74a45df02da02269e4c4a392fcc5b1442c6a
amstevenson <addstevenson@hotmail.com>
2021-07-14 11:24:00 +0100

    js changes

--- src/main/assets/js/courts.ts
+++ src/main/assets/js/courts.ts
@@ -9,6 +9,16 @@ export class CourtsController {
   private courtsHiddenId = 'courtsHidden';
   // private courtsHiddenFilteredId = 'courtsHiddenFiltered';
   private courtsResults = 'courtsResults';
+  private TABLE_HEADER = '    <table class="govuk-table" id="courtsResults">' +
+    '      <thead class="govuk-table__head">' +
+    '        <tr class="govuk-table__row">' +
+    '          <th scope="col" class="govuk-table__header">Name</th>' +
+    '          <th scope="col" class="govuk-table__header">Last Updated</th>' +
+    '          <th scope="col" class="govuk-table__header"></th>' +
+    '          <th scope="col" class="govuk-table__header"></th>' +
+    '        </tr>' +
+    '      </thead>';
+  private TABLE_BODY_START = ' <tbody class="govuk-table__body">';
 
   constructor() {
     this.initialize();
@@ -53,20 +63,11 @@ export class CourtsController {
 
     // const filterValue = $(this.searchCourtsFilterId).val();
     //
-    const tableHeader = '    <table class="govuk-table" id="courtsResults">' +
-    '      <thead class="govuk-table__head">' +
-    '        <tr class="govuk-table__row">' +
-    '          <th scope="col" class="govuk-table__header">Name</th>' +
-    '          <th scope="col" class="govuk-table__header">Last Updated</th>' +
-    '          <th scope="col" class="govuk-table__header"></th>' +
-    '          <th scope="col" class="govuk-table__header"></th>' +
-    '        </tr>' +
-    '      </thead>';
 
-    let tableBody = tableHeader +
-    ' <tbody class="govuk-table__body">';
+    let tableBody = this.TABLE_HEADER + this.TABLE_BODY_START;
 
-    $.each(JSON.parse(courts),function(index,value){
+    $.each(JSON.parse(courts).sort((a: { name: string }, b: { name: string }) =>
+      (a.name > b.name) ? 1 : -1),function(index,value) {
 
       // if(filterValue && value.name.toLowerCase().includes(filterValue.toString().toLowerCase())) {
       //   console.log('match found!: ' + value);
@@ -74,7 +75,7 @@ export class CourtsController {
       // }
 
       const name = value.name;
-      const updatedAt = value.updated_at;
+      const updatedAt = value.updated_at ?? '';
       // const displayed = value.displayed;
       const slug = value.slug;
 
@@ -93,5 +94,12 @@ export class CourtsController {
     tableBody += '</tbody> </table>';
 
     (document.getElementById(this.courtsResults) as HTMLElement).outerHTML = tableBody;
+  }
+
+  private parse(str) {
+    let args = [].slice.call(arguments, 1),
+      i = 0;
+
+    return str.replace(/%s/g, () => args[i++]);
   }
 }
