9c27e08f1de5f20035292ec24425f23547ea5023
amstevenson <addstevenson@hotmail.com>
2021-07-20 13:12:07 +0100

    adding doc strings

--- src/main/assets/js/courts-table-search.ts
+++ src/main/assets/js/courts-table-search.ts
@@ -17,12 +17,20 @@ export class CourtsTableSearch {
   private static tableCourtsNameId = '#tableCourtsName';
   private static tableCourtsUpdatedId = '#tableCourtsUpdated';
 
+  /**
+   *
+   * Sets up the table view for the main page, on load the default configuration
+   * is used, otherwise it is updated on each search toggle or courts search
+   *
+   * @param filterName Either 'name' or 'date' depending on the type of toggle
+   * @param defaultValue set to false by default on initial load
+   */
   static setUpTable(filterName: string, defaultValue = false): void {
-
     defaultValue
       ? CourtsTableSearch.setUpTableData(
         (document.getElementById(this.courtsHiddenId) as HTMLInputElement).value,
-        '', false, orderToggleState.ASC, orderToggleState.INACTIVE)
+      $(this.searchCourtsFilterId).val() as string, // if the user clicks 'back' in the browser after view or editing
+      false, orderToggleState.ASC, orderToggleState.INACTIVE)
       : CourtsTableSearch.setUpTableData((
         document.getElementById(this.courtsHiddenId) as HTMLInputElement).value,
         $(this.searchCourtsFilterId).val() as string,
@@ -37,6 +45,18 @@ export class CourtsTableSearch {
         $(this.tableCourtsUpdatedId), $(this.tableCourtsNameId));
   }
 
+  /**
+   *
+   * Retrieves the filtered results from the toggles and search, and sets the html
+   * of the tbody element of the table accordingly
+   *
+   * @param courts the list of public court information
+   * @param searchFilterValue the search value from the 'search courts or tribunals' input field
+   * @param includeClosedCourts whether or not to include closed courts in the result
+   * @param orderNameAscendingFilter whether to sort by name asc or desc
+   * @param orderUpdatedAscendingFilter whether to sort by last updated date asc or desc
+   * @private
+   */
   private static setUpTableData(courts: string, searchFilterValue: string, includeClosedCourts: boolean,
     orderNameAscendingFilter: string, orderUpdatedAscendingFilter: string): void {
     const filteredCourts = CourtsTableSearch.filterCourts(courts, searchFilterValue, includeClosedCourts,
@@ -47,6 +67,21 @@ export class CourtsTableSearch {
       : $(this.numberOfCourts).hide();
   }
 
+  /**
+   *
+   * Takes a list of courts, and filters based on a set list of criteria:
+   * 1. Name Asc/Desc
+   * 2. Updated date Asc/Desc
+   * 3. Resets whichever one is not enabled
+   * 4. Based on the above, also filters the result list based on the search string from the 'search' input field
+   *
+   * @param courts the list of public courts
+   * @param searchFilterValue the string value of the search input field
+   * @param includeClosedCourts whether or not to include closed courts in the result
+   * @param orderNameAscendingFilter whether to sort by name asc or desc
+   * @param orderUpdatedAscendingFilter whether to sort by last updated date asc or desc
+   * @private
+   */
   private static filterCourts(courts: string, searchFilterValue: string, includeClosedCourts: boolean,
     orderNameAscendingFilter: string, orderUpdatedAscendingFilter: string): any {
 
@@ -72,7 +107,6 @@ export class CourtsTableSearch {
           default:
             break;
         }
-
         switch (orderUpdatedAscendingFilter) {
           case orderToggleState.ASC:
             return (Date.parse(a.updated_at) > Date.parse(b.updated_at)) ? 1 : -1;
@@ -84,6 +118,14 @@ export class CourtsTableSearch {
       }));
   }
 
+  /**
+   *
+   * Create and return the tbody of the table based on a list of table row elements that are created
+   * by looping through each filtered court
+   *
+   * @param filteredCourts the filtered list of public courts
+   * @private
+   */
   private static getCourtsTableBody(filteredCourts: any): string {
     let tableData = '';
     $.each(filteredCourts,function(index,value) {
@@ -100,7 +142,6 @@ export class CourtsTableSearch {
           ' </td>' +
           ' </tr>';
       }
-
       tableData += getDataStructure(value.name, value.updated_at ?? '',
         value.displayed, value.slug, $(CourtsTableSearch.courtsFrontendUrl).val() as string);
     });
@@ -108,6 +149,14 @@ export class CourtsTableSearch {
     return tableData;
   }
 
+  /**
+   *
+   * Switch what toggle is active; i.e if name is clicked on, updated date will
+   * be reset to default and vise versa
+   *
+   * @param toToggle the jquery element parameter to toggle
+   * @param toReset the jquery element parameter to reset
+   */
   static switchTableToggle(toToggle: JQuery, toReset: JQuery): void {
     const toggleUpdatedVal = toToggle.val();
 
@@ -131,8 +180,17 @@ export class CourtsTableSearch {
     toReset.val(orderToggleState.INACTIVE);
   }
 
+  /**
+   *
+   * Based on the toggle state, alter the visual symbol that appears to the side of the column
+   * heading
+   *
+   * @param tableState the state of the clicked on filter (name or updated date)
+   * @param thToToggle the jquery element parameter to toggle the visual symbol of
+   * @param thToReset the jquery element parameter to reset the visual symbol of
+   * @private
+   */
   private static switchTableClasses(tableState: JQuery, thToToggle: JQuery, thToReset: JQuery): void {
-
     switch (tableState.val()) {
       case orderToggleState.ASC: {
         thToToggle.removeClass(this.courtsTableHeaderInactive)
