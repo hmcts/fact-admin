4aa31d1b83c1ea8709f123f50480ca19cb431b2d
amstevenson <addstevenson@hotmail.com>
2021-12-14 11:25:00 +0000

    test

--- src/main/app/controller/courts/NewCourtController.ts
+++ src/main/app/controller/courts/NewCourtController.ts
@@ -8,18 +8,30 @@ export class NewCourtController {
 
   addNewCourtErrorMsg = 'A problem occurred when adding the new court';
 
+  // Need to have the render separately
+  // This method renders the header/footer and the div, and the subsequent one will constitute the content
   public async get(req: AuthedRequest,
     res: Response,
     created = false,
     redirectUrl: string,
     errors: string[] = []): Promise<void> {
-
-    console.log('goes into GET data controller');
-
     res.render('courts/addNewCourt', {
       created: created,
       redirectUrl: redirectUrl,
       errors: errors,
+      csrfToken: CSRF.create()
+    });
+  }
+
+  public async getNewCourtData(req: AuthedRequest,
+    res: Response): Promise<void> {
+
+    console.log('goes into GET data controller');
+
+    res.render('courts/addNewCourtContent', {
+      created: false,
+      redirectUrl: '/courts',
+      errors: [],
       csrfToken: CSRF.create()
     });
   }
@@ -37,7 +49,7 @@ export class NewCourtController {
 
     if(CSRF.verify(req.body._csrf)) {
       console.log('goes into csrf failure');
-      return this.get(req, res, false,'', [this.addNewCourtErrorMsg]);
+      return this.get(req, res, false, '', [this.addNewCourtErrorMsg]);
     }
 
     console.log('gets after csrf check');
@@ -45,7 +57,7 @@ export class NewCourtController {
     const newCourtName = req.body.newCourtName;
     console.log(newCourtName);
 
-    const updatedSlug = newCourtName.name.toLowerCase()
+    const updatedSlug = newCourtName.toLowerCase()
       .replace(/[^\w\s]|_/g, '').split(' ').join('-');
 
     // Check there are no invalid special characters, if so, return with an error
@@ -59,7 +71,7 @@ export class NewCourtController {
     // make call to the api to add a new court
     // make sure the redirect on the javascript forwards on to the /courts/{slug}/generalinfo bit
     // unit tests etc
-    // functional tests 
+    // functional tests
 
     return this.get(req, res, true,'/courts/' + updatedSlug + '/edit#general', []);
   }
