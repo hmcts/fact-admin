b8ab2ad16d23a0779dd40da44306f9fba05e85ea
amstevenson <addstevenson@hotmail.com>
2021-12-30 18:34:50 +0000

    adding constraint on js to check for form element

--- src/main/assets/js/add-new-court.ts
+++ src/main/assets/js/add-new-court.ts
@@ -3,22 +3,26 @@ import {Utilities} from './utilities';
 
 export class NewCourtController {
 
+  private formId = '#addNewCourtForm';
+
   constructor() {
     this.initialise();
   }
 
   private initialise(): void {
     $(() => {
-      $(function() {
-        const redirectUrl = $('#saveNewCourtBtn').attr('href');
-        if (redirectUrl !== undefined && redirectUrl !== '') {
-          // Disable form, and wait for a few seconds before redirecting to the new edit page
-          Utilities.toggleTabEnabled('#addNewCourtForm', false);
-          setTimeout(function(){
-            window.location.href = redirectUrl;
-          }, 3000);
-        }
-      });
+      if ($(this.formId).length > 0) {
+        $(function () {
+          const redirectUrl = $('#saveNewCourtBtn').attr('href');
+          if (redirectUrl !== undefined && redirectUrl !== '') {
+            // Disable form, and wait for a few seconds before redirecting to the new edit page
+            Utilities.toggleTabEnabled('#addNewCourtForm', false);
+            setTimeout(function () {
+              window.location.href = redirectUrl;
+            }, 3000);
+          }
+        });
+      }
     });
   }
 }
