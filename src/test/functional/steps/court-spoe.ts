354052eec33780f47ed50efde5977d0f62c9b7b4
Majid-justice <Majid.Najib@justice.gov.uk>
2022-02-10 16:24:59 +0000

    refactoring spoe functional test steps

--- src/test/functional/steps/court-spoe.ts
+++ src/test/functional/steps/court-spoe.ts
@@ -5,33 +5,26 @@ import {FunctionalTestHelpers} from '../utlis/helpers';
 
 When('I click spoe tab', async () => {
   const selector = '#tab_spoe';
-  const elementExist = await I.checkElement(selector);
-  expect(elementExist).equal(true);
+  expect(await I.checkElement(selector)).equal(true);
   await I.click(selector);
 });
 
 Then('I can view the spoe areas of law listed', async () => {
-  const elementExist = await I.isElementVisible('#spoeForm');
-  expect(elementExist).equal(true);
+  expect(await I.isElementVisible('#spoeForm')).equal(true);
 });
 
 When('I select spoe areas of law {string} and {string}', async (areaOfLaw1: string, areaOfLaw2: string) => {
   const selectorAolAdoption = '#' + areaOfLaw1;
   const selectorAolChildren = '#' + areaOfLaw2;
-  const element1Exist = await I.checkElement(selectorAolAdoption);
-  expect(element1Exist).equal(true);
+  expect(await I.checkElement(selectorAolAdoption)).equal(true);
   const element1Checked = await I.isElementChecked(selectorAolAdoption);
-
-  console.log('..............................element1check boolean...........' + element1Checked);
 
   if (!element1Checked) {
     await I.click(selectorAolAdoption);
   }
-  const element2Exist = await I.checkElement(selectorAolChildren);
-  expect(element2Exist).equal(true);
+  expect(await I.checkElement(selectorAolChildren)).equal(true);
   const element2Checked = await I.isElementChecked(selectorAolChildren);
 
-  console.log('..............................element2check boolean...........' + element2Checked);
   if (!element2Checked) {
     await I.click(selectorAolChildren);
   }
@@ -50,27 +43,21 @@ Then('Success message is displayed for spoe with summary {string}', async (succe
 Then('spoe area of law  {string} and {string} should be selected', async (areaOfLaw1: string, areaOfLaw2: string) => {
   const selector1 = '#' + areaOfLaw1;
   const selector2 = '#' + areaOfLaw2;
-  const element1Exist = await I.checkElement(selector1);
-  expect(element1Exist).equal(true);
-  const element1Checked = await I.isElementChecked(selector1);
-  expect(element1Checked).equal(true);
-  const element2Exist = await I.checkElement(selector2);
-  expect(element2Exist).equal(true);
-  const element2Checked = await I.isElementChecked(selector2);
-  expect(element2Checked).equal(true);
+  expect(await I.checkElement(selector1)).equal(true);
+  expect(await I.isElementChecked(selector1)).equal(true);
+  expect(await I.checkElement(selector2)).equal(true);
+  expect(await I.isElementChecked(selector2)).equal(true);
 });
 
 When('I unselect spoe area of law {string} and {string}', async (areaOfLaw1: string, areaOfLaw2: string) => {
   const selector1 = '#' + areaOfLaw1;
   const selector2 = '#' + areaOfLaw2;
-  const element1Exist = await I.checkElement(selector1);
-  expect(element1Exist).equal(true);
+  expect(await I.checkElement(selector1)).equal(true);
   const element1Checked = await I.isElementChecked(selector1);
   if (element1Checked) {
     await I.click(selector1);
   }
-  const element2Exist = await I.checkElement(selector2);
-  expect(element2Exist).equal(true);
+  expect(await I.checkElement(selector2)).equal(true);
   const element2Checked = await I.isElementChecked(selector2);
   if (element2Checked) {
     await I.click(selector2);
@@ -80,12 +67,9 @@ When('I unselect spoe area of law {string} and {string}', async (areaOfLaw1: str
 Then('spoe areas of law {string} and {string} should be unselected', async (areaOfLaw1: string, areaOfLaw2: string) => {
   const selector1 = '#' + areaOfLaw1;
   const selector2 = '#' + areaOfLaw2;
-  const element1Exist = await I.checkElement(selector1);
-  expect(element1Exist).equal(true);
-  const element1Checked = await I.isElementChecked(selector1);
-  expect(element1Checked).equal(false);
-  const element2Exist = await I.checkElement(selector2);
-  expect(element2Exist).equal(true);
-  const element2Checked = await I.isElementChecked(selector2);
-  expect(element2Checked).equal(false);
+
+  expect(await I.checkElement(selector1)).equal(true);
+  expect(await I.isElementChecked(selector1)).equal(false);
+  expect(await I.checkElement(selector2)).equal(true);
+  expect(await I.isElementChecked(selector2)).equal(false);
 });
