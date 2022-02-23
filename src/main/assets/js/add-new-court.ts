798451c779faaee35eed7a902b700cc96c8ba5ff
amstevenson <addstevenson@hotmail.com>
2022-02-23 17:02:40 +0000

    updating add court to add service area

--- src/main/assets/js/add-new-court.ts
+++ src/main/assets/js/add-new-court.ts
@@ -12,15 +12,32 @@ export class NewCourtController {
   private initialise(): void {
     $(() => {
       if ($(this.formId).length > 0) {
+        const addNewCourtForm = '#addNewCourtForm';
+        const serviceCentreSelection = '#serviceCentreSelection';
+        const serviceCentre = '#serviceCentre';
+        const serviceCentre2 = '#serviceCentre-2';
+        const serviceAreasContainer = '#serviceAreasContainer';
+
         $(function () {
           const redirectUrl = $('#saveNewCourtBtn').attr('href');
           if (redirectUrl !== undefined && redirectUrl !== '') {
             // Disable form, and wait for a few seconds before redirecting to the new edit page
-            Utilities.toggleTabEnabled('#addNewCourtForm', false);
+            Utilities.toggleTabEnabled(addNewCourtForm, false);
             setTimeout(function () {
               window.location.href = redirectUrl;
             }, 3000);
           }
+          // Base selection on whether to hide the service area list or not
+          if ($(serviceCentreSelection).val() === 'false') {
+            console.log('is false');
+            $(serviceAreasContainer).hide();
+          }
+          $(serviceCentre).on('change', () => {
+            $(serviceAreasContainer).toggle();
+          });
+          $(serviceCentre2).on('change', () => {
+            $(serviceAreasContainer).toggle();
+          });
         });
       }
     });
