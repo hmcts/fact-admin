494297da53b7ee4417d1825338c29a4dbfb480bc
amstevenson <addstevenson@hotmail.com>
2022-03-14 16:40:48 +0000

    updating port for smoke test revert

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  TEST_URL: process.env.TEST_URL || 'http://localhost:3120',
+  TEST_URL: process.env.TEST_URL || 'http://localhost:3300',
   TestHeadlessBrowser: true,
   TestSlowMo: 250
 };
