fbb10b28b42aacbc582d2be06d71ee66fa21717f
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-03-22 12:14:31 +0000

    add update functionality for application updates page

--- src/main/app/controller/courts/ApplicationProgressionController.ts
+++ src/main/app/controller/courts/ApplicationProgressionController.ts
@@ -1,23 +1,22 @@
 import autobind from 'autobind-decorator';
 import {AuthedRequest} from '../../../types/AuthedRequest';
 import {Response} from 'express';
-//import {SelectItem} from '../../../types/CourtPageData';
-//import {EmailType} from '../../../types/EmailType';
-//import {validateDuplication, validateEmailFormat} from '../../../utils/validation';
-//import {CSRF} from '../../../modules/csrf';
-//import {Error} from '../../../types/Error';
 import {ApplicationProgression, ApplicationProgressionData} from '../../../types/ApplicationProgression';
 import {Error} from '../../../types/Error';
+import {CSRF} from '../../../modules/csrf';
+import {validateDuplication} from '../../../utils/validation';
 //import {app} from "../../../server";
 
 @autobind
 export class ApplicationProgressionController {
 
-  emptyTypeOrAddressErrorMsg = 'Description and address are required for all emails.';
-  updateErrorMsg = 'A problem occurred when saving the emails.';
+  error1 = 'broken here';
+  error2 = 'nope broken here';
+  emptyTypeErrorMsg = 'Type is needed for all application progressions.';
+  updateErrorMsg = 'A problem occurred when saving the application progression.';
   emailDuplicatedErrorMsg = 'All email addresses must be unique.'
-  getEmailsErrorMsg = 'A problem occurred when retrieving the emails.';
-  getEmailTypesErrorMsg = 'A problem occurred when retrieving the email descriptions.';
+  getApplicationUpdatesErrorMsg = 'A problem occurred when retrieving the application progressions.';
+  //getEmailTypesErrorMsg = 'A problem occurred when retrieving the email descriptions.';
   getEmailAddressFormatErrorMsg = 'Enter an email address in the correct format, like name@example.com';
 
   public async get(
@@ -33,7 +32,7 @@ export class ApplicationProgressionController {
       // Get application updates from API and set the isNew property to false on all application update entries.
       await req.scope.cradle.api.getApplicationUpdates(slug)
         .then((value: ApplicationProgression[]) => applicationUpdates = value.map(e => { e.isNew = false; return e; }))
-        .catch(() => errorMsg.push(this.getEmailsErrorMsg));
+        .catch(() => errorMsg.push(this.getApplicationUpdatesErrorMsg));
     }
 
     if (!applicationUpdates?.some(e => e.isNew === true)) {
@@ -56,12 +55,60 @@ export class ApplicationProgressionController {
     res.render('courts/tabs/applicationProgressionContent', pageData);
   }
 
+  public async put(req: AuthedRequest, res: Response): Promise<void> {
+    let applicationUpdates = req.body.applicationUpdate as ApplicationProgression[] ?? [];
+    applicationUpdates.forEach(e => e.isNew = (e.isNew === true) || ((e.isNew as any) === 'true'));
+
+    if (!CSRF.verify(req.body._csrf)) {
+      return this.get(req, res, false, [this.error1], applicationUpdates);
+    }
+
+    // Remove fully empty entries
+    applicationUpdates = applicationUpdates.filter(e => !this.applicationProgressionEntryIsEmpty(e));
+
+    const errorMsg = this.getErrorMessages(applicationUpdates);
+    if (errorMsg.length > 0) {
+      return this.get(req, res, false, errorMsg, applicationUpdates);
+    }
+
+    await req.scope.cradle.api.updateApplicationUpdates(req.params.slug, applicationUpdates)
+      .then((value: ApplicationProgression[]) => this.get(req, res, true, [], value))
+      .catch((err: any) => this.get(req, res, false, [this.error2], applicationUpdates));
+  }
+
+
   private addEmptyFormsForNewEntries(applicationUpdates: ApplicationProgression[], numberOfForms = 1): void {
     if (applicationUpdates) {
       for (let i = 0; i < numberOfForms; i++) {
         applicationUpdates.push({ type: null, email: null, external_link: null, external_link_description: null, isNew: true });
       }
     }
+  }
+
+  private applicationProgressionEntryIsEmpty(applicationUpdates: ApplicationProgression): boolean {
+    return (!applicationUpdates.type && !applicationUpdates.email?.trim() && !applicationUpdates.external_link?.trim() && !applicationUpdates.external_link_description?.trim());
+  }
+
+  private getErrorMessages(applicationUpdates: ApplicationProgression[]): string[] {
+    const errorMsg: string[] = [];
+    if (applicationUpdates.some(ot => !ot.type)) {
+      // Retains the posted email data when errors exist
+      errorMsg.push(this.emptyTypeErrorMsg);
+    }
+
+    // If any address used is not of an email format, return with an error
+    //if (!validateEmailFormat(applicationUpdates)) {
+    //  errorMsg.push(this.getEmailAddressFormatErrorMsg);
+    //}
+
+    if (!validateDuplication(applicationUpdates, this.emailsDuplicated)) {
+      errorMsg.push(this.emailDuplicatedErrorMsg);
+    }
+    return errorMsg;
+  }
+
+  private emailsDuplicated(applicationUpdates: ApplicationProgression[], index1: number, index2: number): boolean {
+    return applicationUpdates[index1].email && applicationUpdates[index1].email.toLowerCase() === applicationUpdates[index2].email.toLowerCase();
   }
 
 }
