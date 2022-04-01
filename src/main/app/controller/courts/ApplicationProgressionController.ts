e108b1cb6d74c47271872cceb6fc682bb228b4b3
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-04-01 14:04:50 +0100

    add email and url validation

--- src/main/app/controller/courts/ApplicationProgressionController.ts
+++ src/main/app/controller/courts/ApplicationProgressionController.ts
@@ -4,20 +4,20 @@ import {Response} from 'express';
 import {ApplicationProgression, ApplicationProgressionData} from '../../../types/ApplicationProgression';
 import {Error} from '../../../types/Error';
 import {CSRF} from '../../../modules/csrf';
-import {validateDuplication} from '../../../utils/validation';
-//validateEmailFormat
+import {validateDuplication, validateStringEmailFormat, validateUrlFormat} from '../../../utils/validation';
+
 @autobind
 export class ApplicationProgressionController {
 
-  error1 = 'error with CSRF';
-  error2 = 'error with PUT';
   emptyTypeErrorMsg = 'Type is needed for all application progressions.';
   emptyFieldsErrorMsg = 'Enter an email address or an external link';
   linkFieldsErrorMsg = 'Description and link are required to add an external link';
   updateErrorMsg = 'A problem occurred when saving the application progression.';
   emailDuplicatedErrorMsg = 'All email addresses must be unique.'
   getApplicationUpdatesErrorMsg = 'A problem occurred when retrieving the application progressions.';
-  getEmailAddressFormatErrorMsg = 'Enter an email address in the correct format, like name@example.com';
+  invalidEmailFormatErrorMsg = 'Enter an email address in the correct format, like name@example.com';
+  invalidUrlFormatErrorMsg = 'All URLs must be in valid format';
+
 
   public async get(
     req: AuthedRequest,
@@ -59,7 +59,7 @@ export class ApplicationProgressionController {
     applicationProgressions.forEach(e => e.isNew = (e.isNew === true) || ((e.isNew as any) === 'true'));
 
     if (!CSRF.verify(req.body._csrf)) {
-      return this.get(req, res, false, [this.error1], applicationProgressions);
+      return this.get(req, res, false, [this.updateErrorMsg], applicationProgressions);
     }
 
     // Remove fully empty entries
@@ -70,22 +70,10 @@ export class ApplicationProgressionController {
       return this.get(req, res, false, errorMsg, applicationProgressions);
     }
 
-    if (applicationProgressions.some(ap => (!ap.type))){
-      return this.get(req, res, false, [this.emptyTypeErrorMsg], applicationProgressions);
-    }
-    else if (applicationProgressions.some(ap => (ap.type && !ap.email && !ap.external_link && !ap.external_link_description))){
-      return this.get(req, res, false, [this.emptyFieldsErrorMsg], applicationProgressions);
-    }
-    else if (applicationProgressions.some(ap => (ap.external_link && !ap.external_link_description || !ap.external_link && ap.external_link_description))){
-      return this.get(req, res, false, [this.linkFieldsErrorMsg], applicationProgressions);
-
-    } else {
-      await req.scope.cradle.api.updateApplicationUpdates(req.params.slug, applicationProgressions)
-        .then((value: ApplicationProgression[]) => this.get(req, res, true, [], value))
-        .catch((err: any) => this.get(req, res, false, [this.error2], applicationProgressions));
-    }
+    await req.scope.cradle.api.updateApplicationUpdates(req.params.slug, applicationProgressions)
+      .then((value: ApplicationProgression[]) => this.get(req, res, true, [], value))
+      .catch((err: any) => this.get(req, res, false, [this.updateErrorMsg], applicationProgressions));
   }
-
 
   private addEmptyFormsForNewEntries(applicationProgressions: ApplicationProgression[], numberOfForms = 1):
     void {
@@ -109,10 +97,38 @@ export class ApplicationProgressionController {
       errorMsg.push(this.emptyTypeErrorMsg);
     }
 
-    // If any address used is not of an email format, return with an error
-    //if (!validateEmailFormat(applicationUpdates)) {
-    //  errorMsg.push(this.getEmailAddressFormatErrorMsg);
-    //}
+    if (applicationProgressions.some(ap => (ap.type && !ap.email && !ap.external_link &&
+      !ap.external_link_description))){
+      errorMsg.push(this.emptyFieldsErrorMsg);
+    }
+
+    if (applicationProgressions.some(ap => (ap.external_link && !ap.external_link_description ||
+      !ap.external_link && ap.external_link_description))){
+      errorMsg.push(this.linkFieldsErrorMsg);
+    }
+
+    // If any email used is not of an email format, return with an error
+    let emailHasInvalidFormat = false;
+    for (const progression of applicationProgressions){
+      if (progression.email && !validateStringEmailFormat(progression.email)){
+        emailHasInvalidFormat = true;
+      }
+    }
+    if (emailHasInvalidFormat){
+      errorMsg.push(this.invalidEmailFormatErrorMsg);
+    }
+
+    // If any link used is not of a url format, return with an error
+    let linkHasInvalidFormat = false;
+    for (const progression of applicationProgressions) {
+      if (progression.external_link && !validateUrlFormat(progression.external_link)) {
+        progression.isInvalidFormat = true;
+        linkHasInvalidFormat = true;
+      }
+    }
+    if (linkHasInvalidFormat) {
+      errorMsg.push(this.invalidUrlFormatErrorMsg);
+    }
 
     if (!validateDuplication(applicationProgressions, this.emailsDuplicated)) {
       errorMsg.push(this.emailDuplicatedErrorMsg);
@@ -127,3 +143,4 @@ export class ApplicationProgressionController {
   }
 
 }
+
