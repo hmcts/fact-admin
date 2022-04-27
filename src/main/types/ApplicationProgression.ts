63d390b8bff24696399360896a79ea2bede8ddc2
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-04-27 13:59:09 +0100

    include functionality to add or amend  welsh translations

--- src/main/types/ApplicationProgression.ts
+++ src/main/types/ApplicationProgression.ts
@@ -3,9 +3,11 @@ import {Element} from "./Element";
 
 export interface ApplicationProgression extends Element{
   type: string;
+  type_cy: string;
   email: string;
   external_link: string;
   external_link_description: string;
+  external_link_description_cy: string;
 }
 
 export interface ApplicationProgressionData {
