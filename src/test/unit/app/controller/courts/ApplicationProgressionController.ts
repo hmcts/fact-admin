799e3845708da73e955e1ca6f8da532b26d4a993
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-06-07 15:47:01 +0100

    adding tests

--- src/test/unit/app/controller/courts/ApplicationProgressionController.ts
+++ src/test/unit/app/controller/courts/ApplicationProgressionController.ts
@@ -5,7 +5,7 @@ import {ApplicationProgression, ApplicationProgressionData} from '../../../../..
 import {ApplicationProgressionController} from '../../../../../main/app/controller/courts/ApplicationProgressionController';
 import {CourtGeneralInfo} from '../../../../../main/types/CourtGeneralInfo';
 
-describe('AdditionalLinksController', () => {
+describe('ApplicationProgressionController', () => {
 
   let mockApi: {
     getApplicationUpdates: () => Promise<ApplicationProgression[]>,
@@ -13,7 +13,9 @@ describe('AdditionalLinksController', () => {
     getGeneralInfo: () => Promise<CourtGeneralInfo>
   };
 
-  const getUpdates: () => ApplicationProgression[] = () => [
+  const controller = new ApplicationProgressionController();
+
+  const applicationUpdates: () => ApplicationProgression[] = () => [
     {
       type: 'type', type_cy:'type_cy', email: 'email', external_link: 'external_link', external_link_description: 'external_link_description', external_link_description_cy: 'external_link_description_cy', isNew: false
     },
@@ -22,8 +24,10 @@ describe('AdditionalLinksController', () => {
     }
   ];
 
-  const updatesWithEmptyEntry: ApplicationProgression[] = getUpdates().concat({type: null, type_cy: null, email: null, external_link: null, external_link_description: null, external_link_description_cy: null, isNew: true});
+  const updatesWithEmptyEntry: ApplicationProgression[] = applicationUpdates().concat({type: null, type_cy: null, email: null, external_link: null, external_link_description: null, external_link_description_cy: null, isNew: true});
+  const updatesWithNewEntry: ApplicationProgression[] = applicationUpdates().concat({type: 'type 3', type_cy: 'type_cy 3', email: 'email 3', external_link: 'external_link 3', external_link_description: 'external_link_desc 3', external_link_description_cy: 'external_link_desc_cy 3', isNew: true});
   const applicationUpdatesPage = 'courts/tabs/applicationProgressionContent';
+
 
   const courtGeneralInfo: CourtGeneralInfo = {
     name: 'court name',
@@ -34,17 +38,16 @@ describe('AdditionalLinksController', () => {
     alert: 'an alert',
     'alert_cy': 'an alert cy',
     'in_person': true,
-    'service_centre': false,
+    'service_centre': true,
     'sc_intro_paragraph': '',
     'sc_intro_paragraph_cy': ''
   };
 
-  const controller = new ApplicationProgressionController();
 
   beforeEach(() => {
     mockApi = {
-      getApplicationUpdates: async (): Promise<ApplicationProgression[]> => getUpdates(),
-      updateApplicationUpdates: async (): Promise<ApplicationProgression[]> => getUpdates(),
+      getApplicationUpdates: async (): Promise<ApplicationProgression[]> => applicationUpdates(),
+      updateApplicationUpdates: async (): Promise<ApplicationProgression[]> => applicationUpdates(),
       getGeneralInfo: async (): Promise<CourtGeneralInfo> => courtGeneralInfo
     };
 
@@ -69,6 +72,44 @@ describe('AdditionalLinksController', () => {
     expect(res.render).toBeCalledWith(applicationUpdatesPage, expectedResults);
   });
 
+  test('Should handle errors when getting application updates from API', async () => {
+    const req = mockRequest();
+    req.params = {
+      slug: 'royal-courts-of-justice'
+    };
+    req.scope.cradle.api = mockApi;
+    req.scope.cradle.api.getApplicationUpdates = jest.fn().mockRejectedValue(new Error('Mock API Error'));
+    const res = mockResponse();
+
+    await controller.get(req, res);
+
+    const expectedResults: ApplicationProgressionData = {
+      application_progression: null,
+      isEnabled: courtGeneralInfo.service_centre,
+      updated: false,
+      errors: [{text: controller.getApplicationUpdatesErrorMsg}]
+    };
+    expect(res.render).toBeCalledWith(applicationUpdatesPage, expectedResults);
+  });
+
+  test('Should not post application updates if CSRF token is invalid', async () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    req.params = {
+      slug: 'royal-courts-of-justice'
+    };
+    req.body = {
+      application_progression: updatesWithNewEntry,
+      '_csrf': CSRF.create()
+    };
+    req.scope.cradle.api = mockApi;
+    req.scope.cradle.api.updateApplicationUpdates = jest.fn().mockReturnValue(res);
+    (CSRF.verify as jest.Mock).mockReturnValue(false);
+
+    await controller.put(req, res);
+
+    expect(mockApi.updateApplicationUpdates).not.toBeCalled();
+  });
 
 
 });
