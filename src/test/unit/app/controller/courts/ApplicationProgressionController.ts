aabeba4e7b6ee068bf1791621736c9992a71c9c1
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-06-09 11:01:01 +0100

    adding tests

--- src/test/unit/app/controller/courts/ApplicationProgressionController.ts
+++ src/test/unit/app/controller/courts/ApplicationProgressionController.ts
@@ -23,7 +23,6 @@ describe('ApplicationProgressionController', () => {
       type: 'type 2', type_cy:'type_cy 2', email: 'email 2', external_link: 'external_link 2', external_link_description: 'external_link_description 2', external_link_description_cy: 'external_link_description_cy 2', isNew: false
     }
   ];
-
   const updatesWithEmptyEntry: ApplicationProgression[] = applicationUpdates().concat({type: null, type_cy: null, email: null, external_link: null, external_link_description: null, external_link_description_cy: null, isNew: true});
   const updatesWithNewEntry: ApplicationProgression[] = applicationUpdates().concat({type: 'type 3', type_cy: 'type_cy 3', email: 'email 3', external_link: 'external_link 3', external_link_description: 'external_link_desc 3', external_link_description_cy: 'external_link_desc_cy 3', isNew: true});
   const applicationUpdatesPage = 'courts/tabs/applicationProgressionContent';
@@ -111,5 +110,84 @@ describe('ApplicationProgressionController', () => {
     expect(mockApi.updateApplicationUpdates).not.toBeCalled();
   });
 
+  test('Should post application updates if the fields are valid', async () => {
+    const res = mockResponse();
+    const req = mockRequest();
+    req.body = {
+      application_progression: updatesWithNewEntry,
+      '_csrf': CSRF.create()
+    };
+    req.params = {
+      slug: 'royal-courts-of-justice'
+    };
+    req.scope.cradle.api = mockApi;
+    req.scope.cradle.api.updateApplicationUpdates = jest.fn().mockResolvedValue(updatesWithNewEntry);
+    await controller.put(req, res);
+
+    // Should call API to save data
+    expect(mockApi.updateApplicationUpdates).toBeCalledWith(req.params.slug, updatesWithNewEntry);
+  });
+
+  test('Should handle multiple errors when updating emails', async () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    const postedUpdates: ApplicationProgression[] = applicationUpdates()
+      .concat({ type: '', type_cy: '', email: 'email 3', external_link: 'external_link 3', external_link_description: 'external_link_desc 3', external_link_description_cy: 'external_link_desc_cy 3', isNew: true })
+      .concat({ type: 'type 4', type_cy: 'type_cy 4', email: '', external_link: '', external_link_description: '', external_link_description_cy: '', isNew: true })
+      .concat({ type: 'type 5', type_cy: 'type_cy 5', email: '', external_link: 'external_link 5', external_link_description: '', external_link_description_cy: '', isNew: true })
+      .concat({ type: 'type 5a', type_cy: 'type_cy 5a', email: '', external_link: '', external_link_description: 'external_link_description 5a', external_link_description_cy: '', isNew: true })
+      .concat({ type: 'type 6', type_cy: 'type_cy 6', email: 'email 6', external_link: 'external_link 6', external_link_description: 'external_link_description 6', external_link_description_cy: '', isNew: true })
+    req.params = {
+      slug: 'royal-courts-of-justice'
+    };
+    req.body = {
+      application_progression: postedUpdates,
+      '_csrf': CSRF.create()
+    };
+    req.scope.cradle.api = mockApi;
+    req.scope.cradle.api.updateApplicationUpdates = jest.fn().mockRejectedValue(new Error('Mock API Error'));
+
+    await controller.put(req, res);
+
+    const expectedResults: ApplicationProgressionData = {
+      application_progression: postedUpdates,
+      isEnabled: true,
+      updated: false,
+      errors: [
+        {text: controller.updateErrorMsg},
+        {text: controller.emptyTypeErrorMsg},
+        {text: controller.emptyFieldsErrorMsg},
+        {text: controller.linkFieldsErrorMsg},
+        {text: controller.doubleInputErrorMsg}
+      ]
+    };
+    expect(res.render).toBeCalledWith(applicationUpdatesPage, expectedResults);
+  });
+
+
+  test('Should handle error with duplicated email addresses when updating application updates (ignoring casing)', async () => {
+    const req = mockRequest();
+    const postedUpdates: ApplicationProgression[] = applicationUpdates().concat(
+      { type: 'type 3', type_cy: 'type_cy 3', email: 'email 3', external_link: 'external_link 3', external_link_description: 'external_link_desc 3', external_link_description_cy: 'external_link_desc_cy 3', isNew: true }
+    );
+    req.params = { slug: 'royal-courts-of-justice' };
+    req.body = {
+      application_progression: postedUpdates,
+      '_csrf': CSRF.create()
+    };
+    req.scope.cradle.api = mockApi;
+    req.scope.cradle.api.updateApplicationUpdates = jest.fn().mockRejectedValue(new Error('Mock API Error'));
+    const res = mockResponse();
+
+    await controller.put(req, res);
+    const expectedResults: ApplicationProgressionData = {
+      application_progression: postedUpdates,
+      isEnabled: true,
+      errors: [{text: controller.emailDuplicatedErrorMsg}, {text: controller.updateErrorMsg}],
+      updated: false
+    };
+
+    expect(res.render).toBeCalledWith(applicationUpdatesPage, expectedResults);
+  });
 
 });
