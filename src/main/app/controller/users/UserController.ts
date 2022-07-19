67d59f716d5c32939fd544f83762c4e4a5ab8dec
Imran Ali <Imran.Ali@justice.gov.uk>
2022-07-19 13:41:41 +0100

    changes to redirect to idam user dashboard

--- src/main/app/controller/users/UserController.ts
+++ src/main/app/controller/users/UserController.ts
@@ -1,12 +1,12 @@
 import {AuthedRequest} from '../../../types/AuthedRequest';
 import {Response} from 'express';
-
+import config from 'config';
 
 export class UserController {
 
   public async get(req: AuthedRequest, res: Response): Promise<void> {
-
-    res.render('users/index');
+    const userDashboard: string = config.get('services.idam.userDashboard');
+    res.redirect(userDashboard);
   }
 
 
