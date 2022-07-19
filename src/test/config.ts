d1b0fdbbd430ea5c9fd3b95824ade97f7657a38d
amstevenson <addstevenson@hotmail.com>
2022-07-19 12:12:26 +0100

    adding smoke test for idam api, enhancing admin one

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3300',
   TestHeadlessBrowser: true,
-  TestSlowMo: 250
+  TestSlowMo: 250,
+  IDAM_HEALTH_URL: 'https://idam-api.aat.platform.hmcts.net/health'
 };
--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -3,11 +3,20 @@ const { config } = require('../config');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 describe('Smoke Test', () => {
-  describe('healthcheck', () => {
+  describe('fact admin health check', () => {
     test('should return status 200', async () => {
-      const response = await superagent.get(config.TEST_URL);
-
+      const response = await superagent.get(config.TEST_URL + '/health');
+      console.log(response);
       expect(response.statusCode).toBe(200);
+      expect(JSON.parse(response.text)['status']).toBe('UP');
+    });
+  });
+
+  describe('idam api health check', () => {
+    test('should return status 200', async () => {
+      const response = await superagent.get(config.IDAM_HEALTH_URL);
+      expect(response.statusCode).toBe(200);
+      expect(JSON.parse(response.text)['status']).toBe('UP');
     });
   });
 });
