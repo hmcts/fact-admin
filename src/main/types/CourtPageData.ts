732069017a96dc37ca3497ad084ad4983f483eb7
Imran Ali <Imran.Ali@justice.gov.uk>
2022-08-23 10:56:53 +0100

    using flags

--- src/main/types/CourtPageData.ts
+++ src/main/types/CourtPageData.ts
@@ -1,8 +1,18 @@
+import * as flags from '../../main/app/feature-flags/flags';
+
 export interface CourtPageData {
   isSuperAdmin: boolean,
   slug: string,
   name: string,
-  csrfToken: string
+  csrfToken: string,
+  featureFlags?: PageFeatureFlags
+}
+
+export interface PageFeatureFlags {
+  values: {
+    [key: string]: boolean;
+  };
+  flags: typeof flags;
 }
 
 export interface SelectItem {
