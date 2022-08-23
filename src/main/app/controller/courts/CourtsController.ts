e1b1fd837f5aab160828a6323562a9016183875d
majid.najib <majid.najib@justice.gov.uk>
2022-08-23 12:14:54 +0100

    courts page error message

--- src/main/app/controller/courts/CourtsController.ts
+++ src/main/app/controller/courts/CourtsController.ts
@@ -1,6 +1,7 @@
 import autobind from 'autobind-decorator';
 import {Response} from 'express';
 import {AuthedRequest} from '../../../types/AuthedRequest';
+import {Error} from '../../../types/Error';
 
 @autobind
 export class CourtsController {
@@ -8,8 +9,12 @@ export class CourtsController {
   /**
    * GET /courts
    */
+  getCourtsErrorMsg = 'A problem occurred when retrieving all court information.';
+
   public async get(req: AuthedRequest, res: Response): Promise<void> {
+    const errors: Error[] = [];
     const courts = await req.scope.cradle.api.getCourts();
-    res.render('courts/courts', { courts });
+    if (courts.length == 0) {errors.push({text: this.getCourtsErrorMsg});}
+    res.render('courts/courts', { courts, errors });
   }
 }
--- src/test/unit/app/controller/courts/CourtsController.ts
+++ src/test/unit/app/controller/courts/CourtsController.ts
@@ -4,16 +4,41 @@ import { CourtsController } from '../../../../../main/app/controller/courts/Cour
 
 describe('CourtsController', () => {
   const controller = new CourtsController();
-  const mockApi = {
+  const mockApiWithCourts = {
+    getCourts: async (): Promise<object[]> => [
+      {
+        'name': 'Admiralty and Commercial Court',
+        'slug': 'admiralty-and-commercial-court',
+        'updated_at': '08 Jul 2022',
+        'displayed': true
+      }]
+  };
+
+  const mockApiWithoutCourts = {
     getCourts: async (): Promise<object[]> => []
   };
 
   test('Should render the courts page', async () => {
     const req = mockRequest();
-    req.scope.cradle.api = mockApi;
+    req.scope.cradle.api = mockApiWithCourts;
 
     const res = mockResponse();
     await controller.get(req, res);
-    expect(res.render).toBeCalledWith('courts/courts', { courts: [] });
+    expect(res.render).toBeCalledWith('courts/courts', { courts: [
+      {
+        'name': 'Admiralty and Commercial Court',
+        'slug': 'admiralty-and-commercial-court',
+        'updated_at': '08 Jul 2022',
+        'displayed': true
+      }], errors : [] });
+  });
+
+  test('Should display error message when api is down', async () => {
+    const req = mockRequest();
+    req.scope.cradle.api = mockApiWithoutCourts;
+
+    const res = mockResponse();
+    await controller.get(req, res);
+    expect(res.render).toBeCalledWith('courts/courts', { courts: [], errors : [{'text': 'A problem occurred when retrieving all court information.'}] });
   });
 });
