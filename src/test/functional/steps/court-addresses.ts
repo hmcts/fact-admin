64b6bdd903abb77e986233475844e647ee486eaa
Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>
2022-09-06 09:22:48 +0100

    changing id's

--- src/test/functional/steps/court-addresses.ts
+++ src/test/functional/steps/court-addresses.ts
@@ -239,9 +239,9 @@ Then('I select yes for area of law and court type', async () => {
 });
 
 Then('I select children and civil from area of law and county court for court type', async () => {
-  const selectorAolChildren = "input[name='secondaryAddressAOLItems'][id='34249']";
-  const selectorAolCivil = "input[name='secondaryAddressAOLItems'][id='34271']";
-  const selectorCounyCourt = "input[name='secondaryAddressCourtItems'][id='11419']";
+  const selectorAolChildren = "input[id$='34249']";
+  const selectorAolCivil = "input[id$='34271']";
+  const selectorCounyCourt = "input[id$='11419']";
   expect(await I.checkElement(selectorAolChildren)).equal(true);
   await I.click(selectorAolChildren);
   expect(await I.checkElement(selectorAolCivil)).equal(true);
