28cf22f1bcdc8a27674fa8333dd3d57e8d463aee
majid.najib <majid.najib@justice.gov.uk>
2022-09-08 12:35:09 +0100

    add unit test

--- src/test/unit/app/controller/audits/AuditController.ts
+++ src/test/unit/app/controller/audits/AuditController.ts
@@ -187,4 +187,13 @@ describe ( 'AuditController', () => {
     };
     expect(res.render).toBeCalledWith('audits/auditContent', expectedResults);
   });
+
+  test('Should send csv ', async () => {
+    const req = mockRequest();
+    req.scope.cradle.api = mockApi;
+
+    const res = mockResponse();
+    await controller.downloadAuditData(req, res);
+    expect(res.send).toBeCalledTimes(1);
+  });
 });
