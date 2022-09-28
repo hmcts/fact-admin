61642be85b28e9f093ae87bd93673d63e53f5e09
amstevenson <addstevenson@hotmail.com>
2022-09-28 12:12:08 +0100

    test

--- src/test/functional/utlis/feature-flag-helper.ts
+++ src/test/functional/utlis/feature-flag-helper.ts
@@ -1,5 +1,4 @@
 import {LaunchDarkly} from '../../../main/app/feature-flags/LaunchDarklyClient';
-import config from 'config';
 import {FeatureFlags} from '../../../main/app/feature-flags/FeatureFlags';
 
 export class FeatureFlagHelper {
@@ -9,12 +8,7 @@ export class FeatureFlagHelper {
   public async init() {
 
     console.log(process.env.LAUNCHDARKLY_SDK_KEY);
-    console.log('inside of init');
-    console.log(config.get('launchDarkly.ldUser'));
-    console.log(config.get('launchDarkly.sdkKey'));
-    const user: string = config.get('launchDarkly.ldUser');
-    const sdkKey: string = config.get('launchDarkly.sdkKey');
-    const launchDarkly = await new LaunchDarkly(user, sdkKey);
+    const launchDarkly = await new LaunchDarkly('fact-admin', process.env.LAUNCHDARKLY_SDK_KEY);
     this.flagValues = await new FeatureFlags(launchDarkly).getAllFlagValues();
     launchDarkly.closeConnection();
   }
