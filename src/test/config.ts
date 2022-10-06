b706905c185131178f11301e8beeb567c12377a4
amstevenson <addstevenson@hotmail.com>
2022-10-06 13:13:46 +0100

    headless

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3300',
-  TestHeadlessBrowser: false,
+  TestHeadlessBrowser: true,
   TestSlowMo: 250,
   IDAM_HEALTH_URL: 'https://idam-api.aat.platform.hmcts.net/health',
   IDAM_USER_DASHBOARD_HEALTH_URL: 'https://idam-user-dashboard.aat.platform.hmcts.net/health',
