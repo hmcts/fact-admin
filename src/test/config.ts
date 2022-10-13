56639ec26da7b0112710bf85257056262c938954
amstevenson <addstevenson@hotmail.com>
2022-10-13 16:21:14 +0100

    test

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3300',
   TestHeadlessBrowser: true,
-  TestSlowMo: 300,
+  TestSlowMo: 500,
   IDAM_HEALTH_URL: 'https://idam-api.aat.platform.hmcts.net/health',
   IDAM_USER_DASHBOARD_HEALTH_URL: 'https://idam-user-dashboard.aat.platform.hmcts.net/health',
   FRONTEND_URL: process.env.FRONTEND_URL || 'https://fact.aat.platform.hmcts.net'
