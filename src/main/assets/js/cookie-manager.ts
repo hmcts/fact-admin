4b4830468de1e25e3adfba21e37cbdd71ca300dc
majid.najib <majid.najib@justice.gov.uk>
2022-10-13 12:41:17 +0100

    update cookie manager

--- src/main/assets/js/cookie-manager.ts
+++ src/main/assets/js/cookie-manager.ts
@@ -10,29 +10,11 @@ cookieManager.init({
       cookies: [
         'i18next',
         '_oauth2_proxy',
-      ]
-    },
-    {
-      categoryName: 'analytics',
-      optional: false,
-      cookies: [
-        '_ga',
-        '_gid',
-        '_gat_UA-'
-      ]
-    },
-    {
-      categoryName: 'apm',
-      optional: false,
-      cookies: [
-        'dtCookie',
-        'dtLatC',
-        'dtPC',
-        'dtSa',
         'rxVisitor',
-        'rxvt'
+        '_ga',
       ]
-    }
+    },
+
   ]
 });
 
