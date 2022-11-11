3dde41e8fa6252940ff3985211cc82343ad94c13
amstevenson <addstevenson@hotmail.com>
2022-11-11 14:33:18 +0000

    code refactor

--- src/main/app/controller/courts/EditCourtController.ts
+++ src/main/app/controller/courts/EditCourtController.ts
@@ -4,6 +4,7 @@ import {AuthedRequest} from '../../../types/AuthedRequest';
 import {CourtPageData} from '../../../types/CourtPageData';
 import {CourtLock} from '../../../types/CourtLock';
 import {CSRF} from '../../../modules/csrf';
+import {getCurrentDatePlusMinutes} from '../../../utils/DateUtils';
 import * as flags from '../../feature-flags/flags';
 import {ALL_FLAGS_FALSE_ERROR} from '../../../utils/error';
 import {TAB_PREFIX} from '../../../utils/flagPrefix';
@@ -12,29 +13,22 @@ import {TAB_PREFIX} from '../../../utils/flagPrefix';
 export class EditCourtController {
   public async get(req: AuthedRequest, res: Response): Promise<void> {
 
-    const featureFlags = await req.scope.cradle.featureFlags.getAllFlagValues();
-    const filteredFlags = Object.fromEntries(Object.entries(featureFlags)
-      .filter(([key]) => key.startsWith(TAB_PREFIX)));
-    const featureFlagsCount = Object.keys(featureFlags).length;
-
     // Check if the court is currently in use by any other user
     const courtLocks = await req.scope.cradle.api.getCourtLocks(req.params.slug);
+    const currentUserEmail = req.session['user']['jwt']['sub'];
     if (courtLocks.length == 0) {
       // If there are no locks, assign the current user to the court
       await req.scope.cradle.api.addCourtLock(req.params.slug, {
-        court_slug: req.params.slug,
-        user_email: req.session['user']['jwt']['sub']
+        court_slug: req.params.slug, user_email: currentUserEmail
       } as CourtLock);
     } else {
       // At the moment, the limit is one lock; but this may be extended in the future.
       // So for now we can check the first user only
-      const currentLockUserEmail = courtLocks[0]['user_email'];
-      const currentUserEmail = req.session['user']['jwt']['sub'];
-      if (currentLockUserEmail != currentUserEmail) {
-        const currentTime = new Date();
-        const lockTimePlusTimeout = new Date((new Date(courtLocks[0]['lock_acquired'])).getTime()
-          + ((config.get('lock.timeout') as number) * 60000));
-        if (currentTime > lockTimePlusTimeout) {
+      if (courtLocks[0]['user_email'] != currentUserEmail) {
+        // if (new Date() > new DateUtils().getCurrentDatePlusMinutes(courtLocks[0]['lock_acquired'],
+        //   config.get('lock.timeout') as number)) {
+        if (new Date() > getCurrentDatePlusMinutes(courtLocks[0]['lock_acquired'],
+          2)) {
           // If the time of their last action would require the lock to be deleted,
           // then remove and transition over to this user instead.
           await req.scope.cradle.api.deleteCourtLocks(req.params.slug, courtLocks[0]['user_email']);
@@ -54,6 +48,15 @@ export class EditCourtController {
         }
       }
     }
+    return await this.renderEditPage(req.scope.cradle.featureFlags, req, res);
+  }
+
+  private async renderEditPage(featureFlagsClient: any, req: any, res: any) {
+
+    const featureFlags = await featureFlagsClient.getAllFlagValues();
+    const filteredFlags = Object.fromEntries(Object.entries(featureFlags)
+      .filter(([key]) => key.startsWith(TAB_PREFIX)));
+    const featureFlagsCount = Object.keys(featureFlags).length;
 
     const pageData: CourtPageData = {
       isSuperAdmin: req.session.user.isSuperAdmin,
