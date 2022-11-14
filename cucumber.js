3b074d1e5eb1d7500b5f204585396b0861114073
Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>
2022-11-14 18:15:14 +0000

    test

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,9 @@
 let common = [
   //'src/test/functional/features/viewer/*.feature',
-  'src/test/functional/features/super_admin/email-addresses.feature',
+  'src/test/functional/features/super_admin/opening-hours.feature',
   'src/test/functional/features/super_admin/facilities.feature',
   'src/test/functional/features/super_admin/postcodes.feature',
-  'src/test/functional/features/admin/courts.feature',
+  'src/test/functional/features/admin/court-urgent.feature',
   '--require-module ts-node/register',
   '--require src/test/functional/**/*.ts',
   '--tags "not @skipped and not @pending"',
