e556a0d6071c418940c45dbe1dcd2068434f169e
amstevenson <addstevenson@hotmail.com>
2022-11-14 15:22:20 +0000

    update to audit for fortify vulnerability

--- src/main/assets/js/audits.ts
+++ src/main/assets/js/audits.ts
@@ -18,16 +18,16 @@ export class AuditController {
   private initialize(): void {
     $(() => {
       if ($(this.auditsPageContent).length > 0) {
-        this.getAuditContent(0, 10, '', '', '', '');
+        this.getAuditContent(0, 10, {});
         this.setUpSearchSubmitEventHandler();
       }
     });
   }
 
-  private getAuditContent(page: number, size: number, courtLocation: string, email: string, dateFrom: string, dateTo: string): void{
+  private getAuditContent(page: number, size: number, dataObj: {}): void{
 
     $.ajax({
-      url: `/audit-data?page=${page}&size=${size}&location=${courtLocation}&email=${email}&dateFrom=${dateFrom}&dateTo=${dateTo}`,
+      url: `/audit-data?page=${page}&size=${size}&${dataObj}`,
       method: 'get',
       success: async (res) => {
         await $(this.auditsPageContent).html(res);
@@ -46,33 +46,16 @@ export class AuditController {
   private setUpAuditPreviousPageHandler(): void {
     $(this.auditsPagePrev).on('click', async e => {
       e.preventDefault();
-      const dataObj = this.getFormAndPageData();
-
-      const username = AuditController.stripScripts(dataObj['searchUser']);
-      const location = AuditController.stripScripts(dataObj['searchLocation']);
-      const dateFrom = AuditController.stripScripts(dataObj['searchDateFrom']);
-      const dateTo = AuditController.stripScripts(dataObj['searchDateTo']);
       const page = $('#currentPage').val() as number;
-
-      await this.getAuditContent(page == 0 ? 0 : page -1, 10,
-        location == 'select-court' ? '' : location,
-        username, dateFrom, dateTo);
+      await this.getAuditContent(page == 0 ? 0 : page -1, 10, $(this.auditsSearchForm).serialize());
     });
   }
 
   private setUpAuditNextPageHandler(): void {
     $(this.auditsPageNext).on('click', async e => {
       e.preventDefault();
-      const dataObj = this.getFormAndPageData();
-
-      const username = AuditController.stripScripts(dataObj['searchUser']);
-      const location = AuditController.stripScripts(dataObj['searchLocation']);
-      const dateFrom = AuditController.stripScripts(dataObj['searchDateFrom']);
-      const dateTo = AuditController.stripScripts(dataObj['searchDateTo']);
-
       await this.getAuditContent(parseInt($('#currentPage').val() as string) + 1, 10,
-        location == 'select-court' ? '' : location,
-        username, dateFrom, dateTo);
+        $(this.auditsSearchForm).serialize());
     });
   }
 
@@ -80,37 +63,7 @@ export class AuditController {
 
     $(this.auditsSearchForm).on('submit', async e => {
       e.preventDefault();
-
-      const dataObj = this.getFormAndPageData();
-
-      const username = AuditController.stripScripts(dataObj['searchUser']);
-      const location = AuditController.stripScripts(dataObj['searchLocation']);
-      const dateFrom = AuditController.stripScripts(dataObj['searchDateFrom']);
-      const dateTo = AuditController.stripScripts(dataObj['searchDateTo']);
-
-      await this.getAuditContent(0, 10, location == 'select-court' ? '' : location,
-        username, dateFrom, dateTo);
+      await this.getAuditContent(0, 10, $(this.auditsSearchForm).serialize());
     });
-  }
-
-  private getFormAndPageData(): any {
-    const formData = $(this.auditsSearchForm).serializeArray() as any, dataObj = {} as any;
-
-    $(formData).each(function(i, field){
-      dataObj[field.name] = field.value;
-    });
-
-    return dataObj;
-  }
-
-  private static stripScripts(s: string) {
-    const div = document.createElement('div');
-    div.innerHTML = s;
-    const scripts = div.getElementsByTagName('script');
-    let i = scripts.length;
-    while (i--) {
-      scripts[i].parentNode.removeChild(scripts[i]);
-    }
-    return div.innerHTML;
   }
 }
