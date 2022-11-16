634056d025680446afc9884ab2e3b66288cf9b02
Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>
2022-11-16 11:53:53 +0000

    test

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,7 @@
 let common = [
   //'src/test/functional/features/viewer/*.feature',
-   'src/test/functional/features/super_admin/*.feature',
-   'src/test/functional/features/admin/*.feature',
+  'src/test/functional/features/super_admin/*.feature',
+  'src/test/functional/features/admin/*.feature',
   '--require-module ts-node/register',
   '--require src/test/functional/**/*.ts',
   '--tags "not @skipped and not @pending"',
