ac96613ea685ec1bdebbec5cc3534aefe9482042
Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>
2022-11-17 13:50:32 +0000

    test

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-  TEST_URL: process.env.TEST_URL || 'https://fact-admin.aat.platform.hmcts.net/',
-  TestHeadlessBrowser: true,
+  TEST_URL: 'https://fact-admin.aat.platform.hmcts.net/',
+  TestHeadlessBrowser: false,
   TestSlowMo: 350,
   IDAM_HEALTH_URL: 'https://idam-api.aat.platform.hmcts.net/health',
   IDAM_USER_DASHBOARD_HEALTH_URL: 'https://idam-user-dashboard.aat.platform.hmcts.net/health',
