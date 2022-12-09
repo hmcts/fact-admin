1d17efd1577e1253c22ac33419ff49ec12d2635a
amstevenson <addstevenson@hotmail.com>
2022-12-09 13:02:04 +0000

    test

--- cucumber-viewer.js
+++ cucumber-viewer.js
@@ -1,7 +1,5 @@
 let common = [
-  // 'src/test/functional/features/viewer/*.feature',
-  // 'src/test/functional/features/super_admin/*.feature',
-  'src/test/functional/features/admin/general-info.feature',
+  'src/test/functional/features/viewer/*.feature',
   '--require-module ts-node/register',
   '--require src/test/functional/**/*.ts',
   '--tags "not @skipped and not @pending"',
