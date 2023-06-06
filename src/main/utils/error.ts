31b51410326a6fe37c3a561d0c3cf7c1029d1c9c
Imran Ali <Imran.Ali@justice.gov.uk>
2023-06-06 11:42:51 +0100

    removed double space

--- src/main/utils/error.ts
+++ src/main/utils/error.ts
@@ -1,4 +1,4 @@
 const IT_HELP_DESK_PHONE_NUMBER = '0203 989 6060';
 export const ALL_FLAGS_FALSE_ERROR = 'All tabs are currently unavailable. Please contact the FaCT Admin Team.';
-export const NO_MATCHING_ROLES_ERROR = 'You are currently unable to view any courts as you do not have the relevant permissions required.  You will need to submit a ServiceNow ticket on the HMCTS IT portal, using the “Report an IT Issue” option. Alternatively, you can call the IT help desk on '+IT_HELP_DESK_PHONE_NUMBER+'.';
+export const NO_MATCHING_ROLES_ERROR = 'You are currently unable to view any courts as you do not have the relevant permissions required. You will need to submit a ServiceNow ticket on the HMCTS IT portal, using the “Report an IT Issue” option. Alternatively, you can call the IT help desk on '+IT_HELP_DESK_PHONE_NUMBER+'.';
 export const GET_COURTS_ERROR = 'A problem occurred when retrieving all court information.';
