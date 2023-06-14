76841026e8d5464da41824805c20591b8313c8c8
majid.najib <majid.najib@justice.gov.uk>
2023-06-14 18:14:26 +0100

    update

--- src/main/app/controller/courts/CourtsController.ts
+++ src/main/app/controller/courts/CourtsController.ts
@@ -2,7 +2,8 @@ import autobind from 'autobind-decorator';
 import {Response} from 'express';
 import {AuthedRequest} from '../../../types/AuthedRequest';
 import {Error} from '../../../types/Error';
-import { NO_MATCHING_ROLES_ERROR} from '../../../utils/error';
+import {Region} from '../../../types/Region';
+import {GET_COURTS_ERROR, NO_MATCHING_ROLES_ERROR} from '../../../utils/error';
 import {ALLOWED_ROLES} from '../../../utils/roles';
 
 @autobind
@@ -11,24 +12,21 @@ export class CourtsController {
   /**
    * GET /courts and /regions
    */
-  getCourtsErrorMsg = 'A problem occurred when retrieving all court information.';
-
   public async get(req: AuthedRequest, res: Response): Promise<void> {
     const errors: Error[] = [];
-    let courts = await req.scope.cradle.api.getCourts();
-    let regions = await req.scope.cradle.api.getRegions();
+    let courts: Array<unknown> = [];
+    let regions: Array<Region> = [];
     const currentRoles = req.appSession['user']['jwt']['roles'] as string[];
-    await req.scope.cradle.api.deleteCourtLocksByEmail(req.appSession.user.sub);
-
-    if (courts.length == 0) {errors.push({text: this.getCourtsErrorMsg});}
-
+    if (currentRoles.some(i => ALLOWED_ROLES.includes(i))) {
+      courts = await req.scope.cradle.api.getCourts();
+      regions = await req.scope.cradle.api.getRegions();
+      await req.scope.cradle.api.deleteCourtLocksByEmail(req.appSession['user']['jwt']['sub']);
+    }
     if (!currentRoles.some(i => ALLOWED_ROLES.includes(i))) {
       errors.push({text: NO_MATCHING_ROLES_ERROR});
-      courts = [];
-      regions = [];
-      res.render('courts/courts', {courts, regions, errors });
+    } else if (courts.length == 0) {
+      errors.push({text: GET_COURTS_ERROR});
     }
-
-    res.render('courts/courts', { courts, regions, errors });
+    res.render('courts/courts', {courts, regions, errors});
   }
 }
