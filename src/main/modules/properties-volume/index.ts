c36b9e828aea70f16adeb2d33e7b7cea4c6f6e60
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2023-10-23 13:18:04 +0100

    yarn3 - Update properties-volume

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -9,31 +9,33 @@ export class PropertiesVolume {
   enableFor(server: Application) {
     if (server.locals.ENV !== 'development') {
       propertiesVolume.addTo(config);
-      set(config, 'services.idam.clientSecret', get(config, 'secrets.fact.oauth-client-secret'));
-      set(config, 'session.redis.key', get(config, 'secrets.fact.redis-access-key'));
-      set(config, 'session.secret', get(config, 'secrets.fact.redis-access-key'));
-      set(config, 'appInsights.instrumentationKey', get(config, 'secrets.fact.AppInsightsInstrumentationKey'));
-      set(config, 'csrf.tokenSecret', get(config, 'secrets.fact.csrf-token-secret'));
-      set(config, 'lock.timeout', get(config, 'secrets.fact.user-lock-timeout'));
-      set(config, 'launchDarkly.sdkKey', get(config, 'secrets.fact.launchdarkly-sdk-key'));
-      set(config, 'services.image-store.account-name', get(config, 'secrets.fact.storage-account-name'));
-      set(config, 'services.image-store.account-key', get(config, 'secrets.fact.storage-account-primary-key'));
+      this.setSecret('secrets.fact.oauth-client-secret', 'services.idam.clientSecret');
+      this.setSecret('secrets.fact.redis-access-key', 'session.secret');
+      this.setSecret('secrets.fact.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
+      this.setSecret('secrets.fact.csrf-token-secret', 'csrf.tokenSecret');
+      this.setSecret('secrets.fact.user-lock-timeout', 'lock.timeout');
+      this.setSecret('secrets.fact.launchdarkly-sdk-key', 'launchDarkly.sdkKey');
+      this.setSecret('secrets.fact.storage-account-name', 'services.image-store.account-name');
+      this.setSecret('secrets.fact.storage-account-primary-key', 'services.image-store.account-key');
     } else {
-      this.setLocalSecret('oauth-client-secret', 'services.idam.clientSecret');
-      this.setLocalSecret('csrf-token-secret', 'csrf.tokenSecret');
-      this.setLocalSecret('user-lock-timeout', 'lock.timeout');
+      this.setLocalSecret('AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
       this.setLocalSecret('launchdarkly-sdk-key', 'launchDarkly.sdkKey');
-      this.setLocalSecret('storage-account-name', 'services.image-store.account-name');
-      this.setLocalSecret('storage-account-primary-key', 'services.image-store.account-key');
+      this.setLocalSecret('oauth-client-secret', 'services.idam.clientSecret');
     }
   }
 
   /**
    * Load a secret from the AAT vault using azure cli
    */
-  private setLocalSecret(secret: string, toPath: string): void {
-    const result = execSync('az keyvault secret show --vault-name fact-aat -o tsv --query value --name ' + secret);
+  private setSecret(fromPath: string, toPath: string): void {
+    if (config.has(fromPath)) {
+      set(config, toPath, get(config, fromPath));
+    }
+  }
 
+  private setLocalSecret(secret: string, toPath: string): void {
+    // Load a secret from the AAT vault using azure cli
+    const result = execSync('az keyvault secret show --vault-name fact-aat -o tsv --query value --name ' + secret);
     set(config, toPath, result.toString().replace('\n', ''));
   }
 }
