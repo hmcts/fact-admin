fd01c260c9176178d78e8338edb674bad236cf38
majid.najib <majid.najib@justice.gov.uk>
2023-12-06 14:26:45 +0000

    change to compare with UTCdate in court lock

--- src/main/utils/DateUtils.ts
+++ src/main/utils/DateUtils.ts
@@ -1,4 +1,17 @@
 export function getCurrentDatePlusMinutes(time: Date, timeToAdd: number): Date {
-  return new Date((new Date(time)).getTime()
+  return new Date((changeDateToUTCDate(time)).getTime()
     + (timeToAdd * 60000));
+}
+
+export function changeDateToUTCDate(date: Date): Date {
+
+  let year = new Date(date).getUTCFullYear();
+  let month = new Date(date).getUTCMonth();
+  let date_ = new Date(date).getUTCDate();
+  let hours = new Date(date).getUTCHours();
+  let minutes = new Date(date).getUTCMinutes();
+  let seconds = new Date(date).getUTCSeconds();
+  let milliseconds = new Date(date).getUTCMilliseconds();
+
+  return new Date(year, month, date_, hours, minutes, seconds, milliseconds);
 }
