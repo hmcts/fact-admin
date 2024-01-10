fe4d14e202107e39a56659fb96e55538103a6d35
Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>
2024-01-10 14:49:26 +0000

    test

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,14 +1,12 @@
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3300',
-
-
   TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
   TestSlowMo: 420,
   IDAM_HEALTH_URL: 'https://idam-api.aat.platform.hmcts.net/health',
   IDAM_USER_DASHBOARD_HEALTH_URL: 'https://idam-user-dashboard.aat.platform.hmcts.net/health',
   FRONTEND_URL: process.env.FRONTEND_URL || 'https://fact.aat.platform.hmcts.net',
   // TODO: FaCT API should really be hosted at 'https://fact-api.aat.platform.hmcts.net', update this when API is migrated
-  API_URL: process.env.API_URL || 'http://fact-api-aat.service.core-compute-aat.internal',
+  API_URL: 'http://fact-api-aat.service.core-compute-aat.internal',
   username: process.env.OAUTH_USER,
   viewerUsername: process.env.OAUTH_VIEWER_USER,
   superUsername: process.env.OAUTH_SUPER_USER,
