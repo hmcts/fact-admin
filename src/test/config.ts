e189ed3baa79d3ec963b050b73c67952e1758168
Josh Blackmoor <joshblackmoor01@icloud.com>
2024-09-05 08:26:12 +0100

    Missed ,

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3300',
-  TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true
+  TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
   TestSlowMo: 420,
   IDAM_HEALTH_URL: 'https://idam-api.aat.platform.hmcts.net/health',
   IDAM_USER_DASHBOARD_HEALTH_URL: 'https://idam-user-dashboard.aat.platform.hmcts.net/health',
