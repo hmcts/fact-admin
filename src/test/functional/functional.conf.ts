8a1d8f3ec8ebc3e8b741f6251c3037262e4c544b
Josh Blackmoor <joshblackmoor01@icloud.com>
2024-09-20 06:22:50 +0100

    poc

---
+++ src/test/functional/functional.conf.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import { config as testConfig } from '../config';
+
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+setHeadlessWhen(testConfig.TestHeadlessBrowser);
+
+export const config: CodeceptJS.MainConfig = {
+  name: 'functional',
+  tests: './tests/**/*-test.ts',
+  output: path.join(testConfig.TestFunctionalOutputPath, 'functional/reports'),
+  include: {
+    I: './tests/custom-steps.ts',
+  },
+  helpers: testConfig.helpers,
+  mocha: {},
+  plugins: {
+    ...testConfig.plugins,
+    pauseOnFail: {
+      enabled: !testConfig.TestHeadlessBrowser,
+    },
+  },
+};
--- src/test/functional/steps.d.ts
+++ src/test/functional/steps.d.ts
@@ -1,13 +1,16 @@
 /// <reference types='codeceptjs' />
-type FactApiHelper = import('./helpers/FactApiHelper');
+type stepsFile = typeof import('./tests/custom-steps.js');
 
 declare namespace CodeceptJS {
   interface SupportObject {
     I: I;
-    current: any
-    login: (role: string) => void
+    current: any;
   }
-  interface I extends WithTranslation<Methods> {}
+
+  interface Methods extends Playwright {}
+
+  interface I extends ReturnType<stepsFile>, WithTranslation<Methods> {}
+
   namespace Translation {
     interface Actions {}
   }
