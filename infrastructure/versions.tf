94787bb2cdde297b5b41a7a73f008ab3928f2715
Danny Furnivall <danielfurnivall@gmail.com>
2025-02-17 11:53:25 +0000

    Test to see if this works

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0"
+    }
+  }
+  required_version = ">= 1.1.0"
 }
