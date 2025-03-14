ab88f8db7284c86371a5ba6fc4d3eaaa19be723f
Imran Ali <Imran.Ali@justice.gov.uk>
2025-03-14 17:06:36 +0000

    replaced lodash

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -1,7 +1,6 @@
 import config from 'config';
 import * as propertiesVolume from '@hmcts/properties-volume';
 import {Application} from 'express';
-import {get, set} from 'lodash';
 import {spawnSync} from 'child_process';
 
 export class PropertiesVolume {
@@ -34,13 +33,42 @@ export class PropertiesVolume {
    */
   private setSecret(fromPath: string, toPath: string): void {
     if (config.has(fromPath)) {
-      set(config, toPath, get(config, fromPath));
+      const value = config[fromPath as keyof typeof config];
+      const keys = toPath.split('.');
+      let target: any = config;
+
+      for (let i = 0; i < keys.length - 1; i++) {
+        if (!(keys[i] in target)) target[keys[i]] = {};
+        target = target[keys[i]];
+      }
+
+      target[keys[keys.length - 1]] = value;
     }
   }
 
   private setLocalSecret(secret: string, toPath: string): void {
     // Load a secret from the AAT vault using azure cli
     const result = spawnSync('az', ['keyvault', 'secret', 'show', '--vault-name', 'fact-aat', '-o', 'tsv', '--query', 'value', '--name', secret], {encoding: 'utf8'});
-    set(config, toPath, encodeURI(result.stdout.replace('\n', '')));
+    this.setDeepValue(config, toPath, encodeURI(result.stdout.trim()));
   }
+
+  /**
+   * Set secret value in config object
+   * @param obj this is the config object
+   * @param path the path to the secret in the config object
+   * @param value the value of the secret
+   */
+  private setDeepValue(obj: Record<string, any>, path: string, value: any): void {
+    const keys = path.split('.');
+    let target = obj;
+
+    for (let i = 0; i < keys.length - 1; i++) {
+      if (!(keys[i] in target) || typeof target[keys[i]] !== 'object') {
+        target[keys[i]] = {};
+      }
+      target = target[keys[i]];
+    }
+    target[keys[keys.length - 1]] = value;
+  }
+
 }
