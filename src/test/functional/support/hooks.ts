447d71c85d9b9189354a6e058127b38b44622145
Daniel Furnivall <danielfurnivall@gmail.com>
2025-03-18 10:25:31 +0000

    fix hooks.ts attempt

--- src/test/functional/support/hooks.ts
+++ src/test/functional/support/hooks.ts
@@ -2,11 +2,17 @@ import {After, AfterAll, Before, BeforeAll, setDefaultTimeout} from 'cucumber';
 import puppeteer from 'puppeteer';
 import {puppeteerConfig} from '../puppeteer.config';
 import {FeatureFlagHelper} from '../utlis/feature-flag-helper';
+import os from 'os';
+import path from 'path';
 
 const scope = require('./scope');
 
 export const launchBrowser = async () => {
-  scope.browser = await puppeteer.launch(puppeteerConfig);
+  const uniqueUserDataDir = path.join(os.tmpdir(), `puppeteer_profile_${Date.now()}`);
+  scope.browser = await puppeteer.launch({
+    ...puppeteerConfig,
+    userDataDir: uniqueUserDataDir
+  });
 };
 
 const f = new FeatureFlagHelper();
@@ -39,10 +45,7 @@ AfterAll(async () => {
 });
 
 Before(async (scenario) => {
-
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const falseTagValues = scenario.pickle.tags.filter(item => !allFlags[item.name.replace('@', '')]);
   return falseTagValues.length > 0 ? 'skipped' : 'run';
-
-});
