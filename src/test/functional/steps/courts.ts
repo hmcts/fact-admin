ed31985dd8e21f2b057473306d4c6bb15231de61
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-08-12 14:39:04 +0100

    align the dash

--- src/test/functional/steps/courts.ts
+++ src/test/functional/steps/courts.ts
@@ -24,7 +24,7 @@ Then('I am redirected to the Edit Court page for the {string}', async (courtName
   const pageTitle = await I.getPageTitle();
   const editCourtHeading = await I.getElement('#court-name');
   const editCourtHeadingText = await I.getElementText(editCourtHeading);
-  expect(pageTitle).equal('Edit ' + courtName + ' - ' + FunctionalTestHelpers.DEPARTMENT_SERVICE);
+  expect(pageTitle).equal('Edit ' + courtName + ' – ' + FunctionalTestHelpers.DEPARTMENT_SERVICE);
   expect(editCourtHeadingText).equal('Editing - ' + courtName);
   await I.checkElementIsAnchor('#courts');
   await I.checkElementIsAnchor('#my-account');
